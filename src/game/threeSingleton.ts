80ab317112e56a07d8f2c07e38e5b509c59cd5f4
Quas! <156349255+Quas-Storage@users.noreply.github.com>
2025-12-13 14:41:42 +0100

    three import fix for vercel

--- src/game/threeSingleton.ts
+++ src/game/threeSingleton.ts
@@ -3,5 +3,5 @@
  * You're not allowed to have multiple imports of three or Three, so
  * we make a singleton of it that gets called everywhere
  * */
-import * as Three from "Three";
+import * as Three from "three";
 export default Three;
